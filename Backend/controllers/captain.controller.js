dabeb1fce3506a682a17a45b39c6290a7c5f82b1
Subhamstar <mr.subhamdas999@gmail.com>
2025-05-12 22:41:19 +0530

    captainController Create

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,1 +1,3 @@
 const captainModel=require('../models/captain.model');
+const captainService=require('../services/captain.service');
+const { createCaptain } = require('../services/captain.service');
